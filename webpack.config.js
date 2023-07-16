624b9e62eeb843779d5d08df7e0f470a95d258d9
Nicolas SERIN <nserin.uniter@gmail.com>
2023-07-16 08:05:33 +0200

    merge branche fixturesconfig yarn

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('main_style', './assets/styles/app.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
