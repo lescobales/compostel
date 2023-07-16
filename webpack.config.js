6ab9db3cb664b27288332fcefcc10047b2bc4acc
Nicolas SERIN <nserin.uniter@gmail.com>
2023-07-16 08:20:56 +0200

    webpack done

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
-    .addEntry('main_style', './assets/styles/app.js')
+    .addEntry('main_script', './assets/scripts/script.js')
+    .addEntry('main_style', './assets/styles/main.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -55,7 +55,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
